6cac68f9f8fdcf5a4058663f732335fffd3381a9
kwamebio <kwameagyemang73@gmail.com>
2024-04-02 22:41:15 +0100

    remove aws config from application.rb

--- config/application.rb
+++ config/application.rb
@@ -33,9 +33,6 @@ module Video
   # config/application.rb or any initializer file
 
 # Print out the values of AWS environment variables
-puts "AWS_ACCESS_KEY_ID: #{ENV['AWS_ACCESS_KEY_ID']}"
-puts "AWS_SECRET_ACCESS_KEY: #{ENV['AWS_SECRET_ACCESS_KEY']}"
-puts "AWS_REGION: #{ENV['AWS_REGION']}"
-puts "AWS_BUCKET_NAME: #{ENV['AWS_BUCKET_NAME']}"
+
 
 end
